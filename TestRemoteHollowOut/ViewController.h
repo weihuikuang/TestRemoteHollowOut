6e6d43c379bf17702661629b956cf92330df5330
weihuikuang <1152807833@qq.com>
2018-05-09 14:18:03 +0800

    第一次远程提交

---
+++ TestRemoteHollowOut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRemoteHollowOut
+//
+//  Created by Handbbc on 2018/5/9.
+//  Copyright © 2018年 shiDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestRemoteHollowOut/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  TestRemoteHollowOut
+//
+//  Created by Handbbc on 2018/5/9.
+//  Copyright © 2018年 shiDong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor cyanColor];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
