6bd308fe41d4f9a70e67ea40f259581e838655da
weihuikuang <1152807833@qq.com>
2018-05-09 15:28:42 +0800

    建立第三个控制器

---
+++ TestRemoteHollowOut/ThreeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThreeViewController.h
+//  TestRemoteHollowOut
+//
+//  Created by Handbbc on 2018/5/9.
+//  Copyright © 2018年 shiDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThreeViewController : UIViewController
+
+@end
---
+++ TestRemoteHollowOut/ThreeViewController.m
@@ -0,0 +1,38 @@
+//
+//  ThreeViewController.m
+//  TestRemoteHollowOut
+//
+//  Created by Handbbc on 2018/5/9.
+//  Copyright © 2018年 shiDong. All rights reserved.
+//
+
+#import "ThreeViewController.h"
+
+@interface ThreeViewController ()
+
+@end
+
+@implementation ThreeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"第三个";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
