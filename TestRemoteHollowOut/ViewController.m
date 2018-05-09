823fd8631e07df1cd9b1efbdc74a414409858f69
weihuikuang <1152807833@qq.com>
2018-05-09 14:30:34 +0800

    视图镂空核心代码

--- TestRemoteHollowOut/ViewController.m
+++ TestRemoteHollowOut/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+
 @interface ViewController ()
 
 @end
@@ -17,7 +20,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor cyanColor];
-    
+    UIImageView *imageView =[[UIImageView alloc] initWithFrame:CGRectMake(0, 64, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    imageView.image = [UIImage imageNamed:@"icon"];
+    [imageView setBackgroundColor:[UIColor grayColor]];
+    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRect:imageView.bounds];
+    UIBezierPath *holePath = [UIBezierPath bezierPathWithRect:CGRectInset(CGRectMake(50, 100, SCREEN_WIDTH-100, SCREEN_WIDTH-100), 0, 0)];
+    [maskPath appendPath:holePath];
+    CAShapeLayer *mask = [CAShapeLayer layer];
+    [mask setFillRule:kCAFillRuleEvenOdd];
+    mask.path = maskPath.CGPath;
+    [imageView.layer setMask:mask];
+    [self.view addSubview:imageView];
 }
 
 
